58b8eb104423a50be0013bdedfcd085ec09f21b7
JuliaPustovaya <pustovayajulia@gmail.com>
2019-08-17 17:32:13 +0300

    Add one-to-one mapping two tables: Student and StudentInfo

---
+++ src/main/java/StudentInformation.java
@@ -0,0 +1,50 @@
+public class StudentInformation {
+
+    private Integer studentId;
+    private String address;
+    private String registryNumber;
+    private String phoneNumber;
+
+    private Student student;
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRegistryNumber() {
+        return registryNumber;
+    }
+
+    public void setRegistryNumber(String registryNumber) {
+        this.registryNumber = registryNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+}
